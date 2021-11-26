e2d891fd5e51cf34e354628310cd034bd8cc22a7
Pranali More <morepranali1301@gmail.com>
2021-11-26 19:57:31 +0530

    Uploading First Rest Project

---
+++ RestServiceExample/src/main/java/com/example/RestServiceExample/MainController.java
@@ -0,0 +1,15 @@
+package com.example.RestServiceExample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	  
+	@GetMapping("/get")
+	public String getMsg()
+	{
+		return "Hello";
+	}
+
+}
---
+++ RestServiceExample/src/test/java/com/example/RestServiceExample/RestServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
